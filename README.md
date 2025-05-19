420266c9c43255ac06323a4407975ca703589e51
IBCHgenomic <gsablok@ibch.poznan.pl>
2025-05-19 11:36:55 +0200

    final release

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
-# fedora
+# fedora-devops
 
-![](https://github.com/IBCHgenomic/eVaiutilities/blob/main/logo.png)
+- a fedora system utility for monitoring the fedora installations.
+- all the commands in the install take the flag and the yes and the rpm path.
 
-- fedora system management.
-- fedora systemd implementation in Golang.
+```
+Usage:
+  options run [flags]
 
-- Code related queries: Dr. Gaurav Sablok: gsablok@ibch.poznan.pl.
+Flags:
+  -e, --autoremove string      remove all the dnf install (default "remove dnf")
+  -c, --clean string           clean all the package repositories (default "clean all the packages")
+  -x, --dnfupdate string       update all dnf repositories (default "update all dnf")
+  -h, --help                   help for run
+  -j, --history string         reports history of all the repositories (default "history of the packages")
+  -l, --listcommand string     list all the repositories (default "list dnf")
+  -q, --listrecent string      list all the recent dnf (default "list recent dnf")
+  -L, --lscpu string           lscpu detail (default "check the lscpu")
+  -E, --lscpuextended string   lscpu extended (default "lscpu extended with json write")
+  -m, --minimal string         update all minimal dnf (default "update all dnf minimal")
+  -w, --refresh string         refreshing packages (default "refresh all the packages")
+  -u, --upgrade string         list all the dnf upgrade (default "upgrade dnf")
 
- Gaurav Sablok \
- Instytut Chemii Bioorganicznej \
- Polskiej Akademii Nauk \
- ul. Noskowskiego 12/14 | 61-704, Poznań \
- Poland
+Usage:
+  options install [flags]
+
+Flags:
+  -h, --help                  help for install
+  -i, --info string           package information (default "information about a specific package")
+  -s, --install string        package install (default "install a specific package")
+  -b, --pkgdowngrade string   downgrade a specific package (default "downgrade a specific package")
+  -p, --pkgupdate string      update the dnf packages (default "update the packages")
+  -z, --reinstall string      re-install a specific package (default "reinstall a package")
+  -v, --removep string        remove a specific package (default "remove a specific package")
+  -r, --rollback string       rollback a specific release for the package (default "rolback a specific release")
+  -f, --rpm string            install a specific rpm (default "install a specific rpm")
+  -t, --search string         search for a specific package repository (default "search a specific package")
+  -a, --searchall string      search all the package repository (default "search all the package")
+```
---
+++ main.go
@@ -0,0 +1,289 @@
+package main
+
+/*
+
+Author Gaurav Sablok
+Instytut Chemii Bioorganicznej
+Polskiej Akademii Nauk
+ul. Noskowskiego 12/14 | 61-704, Poznań
+Date: 2025-5-14
+
+a fedora system utility for monitoring the fedora installations in cloud antive applications.
+you can install this as sudo or provide the password for the validation in case of some of the command needs
+a complete fedora dnf system manager written in Golang
+
+*/
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var (
+	list          string
+	listrecent    string
+	upgrade       string
+	autoremove    string
+	dnfupdate     string
+	minimal       string
+	pkgupdate     string
+	pkgdowngrade  string
+	clean         string
+	history       string
+	rollback      string
+	info          string
+	install       string
+	reinstall     string
+	removep       string
+	search        string
+	searchall     string
+	refresh       string
+	alignment     string
+	rpm           string
+	lscpu         string
+	lscpuextended string
+)
+
+var rootCmd = &cobra.Command{
+	Use:  "options",
+	Long: "This is a fedora devops application that allows you to do all the fedora system devops",
+}
+
+var systemCmd = &cobra.Command{
+	Use:  "run",
+	Long: "Only system maintainence task needs to be done with this flag",
+	Run:  systemFunc,
+}
+
+var installCmd = &cobra.Command{
+	Use:  "install",
+	Long: "system wide installation needs to be done with this flag only. Use the -f flag to define the rpm package or the packageid",
+	Run:  installFunc,
+}
+
+func init() {
+	// systemCmd
+	systemCmd.Flags().
+		StringVarP(&list, "listcommand", "l", "list dnf", "list all the repositories")
+	systemCmd.Flags().
+		StringVarP(&listrecent, "listrecent", "q", "list recent dnf", "list all the recent dnf")
+	systemCmd.Flags().
+		StringVarP(&refresh, "refresh", "w", "refresh all the packages", "refreshing packages")
+	systemCmd.Flags().
+		StringVarP(&upgrade, "upgrade", "u", "upgrade dnf", "list all the dnf upgrade")
+	systemCmd.Flags().
+		StringVarP(&autoremove, "autoremove", "e", "remove dnf", "remove all the dnf install")
+	systemCmd.Flags().
+		StringVarP(&dnfupdate, "dnfupdate", "x", "update all dnf", "update all dnf repositories")
+	systemCmd.Flags().
+		StringVarP(&minimal, "minimal", "m", "update all dnf minimal", "update all minimal dnf")
+	installCmd.Flags().
+		StringVarP(&pkgupdate, "pkgupdate", "p", "update the packages", "update the dnf packages")
+	installCmd.Flags().
+		StringVarP(&pkgdowngrade, "pkgdowngrade", "b", "downgrade a specific package", "downgrade a specific package")
+	systemCmd.Flags().
+		StringVarP(&clean, "clean", "c", "clean all the packages", "clean all the package repositories")
+	systemCmd.Flags().
+		StringVarP(&history, "history", "j", "history of the packages", "reports history of all the repositories")
+	installCmd.Flags().
+		StringVarP(&rollback, "rollback", "r", "rolback a specific release", "rollback a specific release for the package")
+	installCmd.Flags().
+		StringVarP(&info, "info", "i", "information about a specific package", "package information")
+	installCmd.Flags().
+		StringVarP(&install, "install", "s", "install a specific package", "package install")
+	installCmd.Flags().
+		StringVarP(&reinstall, "reinstall", "z", "reinstall a package", "re-install a specific package")
+	installCmd.Flags().
+		StringVarP(&removep, "removep", "v", "remove a specific package", "remove a specific package")
+	installCmd.Flags().
+		StringVarP(&search, "search", "t", "search a specific package", "search for a specific package repository")
+	installCmd.Flags().
+		StringVarP(&searchall, "searchall", "a", "search all the package", "search all the package repository")
+	installCmd.Flags().
+		StringVarP(&rpm, "rpm", "f", "install a specific rpm", "install a specific rpm")
+	systemCmd.Flags().StringVarP(&lscpu, "lscpu", "L", "check the lscpu", "lscpu detail")
+	systemCmd.Flags().
+		StringVarP(&lscpuextended, "lscpuextended", "E", "lscpu extended with json write", "lscpu extended")
+
+	rootCmd.AddCommand(systemCmd)
+	rootCmd.AddCommand(installCmd)
+}
+
+func systemFunc(cmd *cobra.Command, args []string) {
+	if list == "yes" {
+		out, err := exec.Command("dnf", "list", "--installed").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+	if refresh == "yes" {
+		out, err := exec.Command("sudo", "dnf", "makecache", "--refresh").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if listrecent == "yes" {
+		out, err := exec.Command("sudo", "dnf", "list", "--recent").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if upgrade == "yes" {
+		out, err := exec.Command("sudo", "dnf", "list", "--upgrade").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if autoremove == "yes" {
+		out, err := exec.Command("sudo", "dnf", "list", "--autoremove").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if dnfupdate == "yes" {
+		out, err := exec.Command("sudo", "dnf", "check-update").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if minimal == "yes" {
+		out, err := exec.Command("sudo", "dnf", "upgrade-minimal").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if clean == "yes" {
+		out, err := exec.Command("sudo", "dnf", "cleanall").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+		out1, err1 := exec.Command("sudo", "dnf", "clean", "metadata").Output()
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		fmt.Println(string(out1))
+	}
+
+	if history == "yes" {
+		out, err := exec.Command("sudo", "dnf", "history").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if lscpu == "yes" {
+		out, err := exec.Command("lscpu").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if lscpuextended == "yes" {
+		out, err := exec.Command("lscpu", "--extended", "--json").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+}
+
+func installFunc(cmd *cobra.Command, args []string) {
+	if removep == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "remove", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if pkgupdate == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "upgrade", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if pkgdowngrade == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "downgrade", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if rollback == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "rollback", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if info == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "info", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if install == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "install", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if search == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "search", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+
+	if searchall == "yes" {
+		rpminstall := rpm
+		out, err := exec.Command("sudo", "dnf", "search", "--all", "-y", rpminstall).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(out))
+	}
+}
